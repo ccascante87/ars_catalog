68f346101e18016c3e7e7c6680be3d141be023fd
Ricardo. <ricforco@gmail.com>
2018-09-05 18:25:50 -0600

    Donut Chart

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ var svg = d3.select("#svgview"),
     width = svg.attr("width") - margin.left - margin.right,
     height = svg.attr("height") - margin.top - margin.bottom;
 
+var pieChartIDName  = "#div_infobox";
+var legendIDName    = "#div_legend";
+
+var fData=[]
+var colorsTargetAddress =[] 
+
 // var x = d3.scaleLinear()
 var x = d3.scalePoint()
     .range([30,width])
@@ -129,7 +135,7 @@ function update(data) {
 
         circle.classed('selected',false)
         node.classed('selected',true)
-        // updateInfo(d)
+        updateInfo(d)
         selected = node
 
         return;
@@ -203,7 +209,7 @@ function update(data) {
 
       // update UI
       d3.select('#closest').html(artworks.map(d => `${d.sourceAddress}`).join(' - '))
-      // updateInfo(d)
+      updateInfo(d)
 
   })
 
@@ -237,6 +243,113 @@ function update(data) {
       .alphaDecay(0.01)
 
   }
+  
+  function segColor(c){ 
+	  var color = '#' + (Math.random().toString(16) + "000000").substring(2,8);
+	  var id = c;
+	  var found = colorsTargetAddress.some(function (el) {
+		console.log(el.color + "+" + c);
+		if( el.id === id)
+			color = el.color;
+	  });
+	  if (!found) { colorsTargetAddress.push({ id: id, color: color }); }	  
+	  
+	  return color;	  
+  }    
+  
+  
+  // function to handle pieChart.
+  function pieChart(id,pD){
+
+	  d3.select(pieChartIDName).html('')	  
+	  d3.select(legendIDName).html('')
+	  
+      var pC ={},    pieDim ={w:250, h: 250};
+      pieDim.r = Math.min(pieDim.w, pieDim.h) / 2;
+      
+      // create svg for pie chart.
+      var piesvg = d3.select(id).append("svg")
+          .attr("width", pieDim.w).attr("height", pieDim.h).append("g")
+          .attr("transform", "translate("+pieDim.w/2+","+pieDim.h/2+")");
+         
+      // create function to draw the arcs of the pie slices.
+      var arc = d3.svg.arc().outerRadius(pieDim.r - 10).innerRadius(pieDim.r - 40);
+      
+      // create a function to compute the pie slice angles.
+      var pie = d3.layout.pie().sort(null).value(function(d) { return d.freq; });
+
+      // Draw the pie slices.
+      piesvg.selectAll("path").data(pie(pD)).enter().append("path").attr("d", arc)
+          .each(function(d) { this._current = d; })
+          .style("fill", function(d) { return segColor(d.data.type); })
+          .on("mouseover",mouseover).on("mouseout",mouseout);
+
+      // create function to update pie-chart. This will be used by histogram.
+      pC.update = function(nD){
+          piesvg.selectAll("path").data(pie(nD)).transition().duration(500)
+              .attrTween("d", arcTween);
+      }        
+      // Utility function to be called on mouseover a pie slice.
+      function mouseover(d){
+
+      }
+      //Utility function to be called on mouseout a pie slice.
+      function mouseout(d){
+      }
+      // Animating the pie-slice requiring a custom function which specifies
+      // how the intermediate paths should be drawn.
+      function arcTween(a) {
+          var i = d3.interpolate(this._current, a);
+          this._current = i(0);
+          return function(t) { return arc(i(t));    };
+      }    
+      return pC;
+  }
+  
+  // function to handle legend.
+  function legend(id,lD){
+      var leg = {};
+          
+      // create table for legend.
+      var legend = d3.select(id).append("table").attr('class','legend');
+      
+      // create one row per segment.
+      var tr = legend.append("tbody").selectAll("tr").data(lD).enter().append("tr");
+          
+      // create the first column for each segment.
+      tr.append("td").append("svg").attr("width", '16').attr("height", '16').append("rect")
+          .attr("width", '16').attr("height", '16')
+			.attr("fill",function(d){ return segColor(d.type); });
+          
+      // create the second column for each segment.
+      tr.append("td").text(function(d){ return d.type;});
+
+      // create the third column for each segment.
+      tr.append("td").attr("class",'legendFreq')
+          .text(function(d){ return d3.format(",")(d.freq);});
+
+      // create the fourth column for each segment.
+      tr.append("td").attr("class",'legendPerc')
+          .text(function(d){ return getLegend(d,lD);});
+
+      // Utility function to be used to update the legend.
+      leg.update = function(nD){
+          // update the data attached to the row elements.
+          var l = legend.select("tbody").selectAll("tr").data(nD);
+
+          // update the frequencies.
+          l.select(".legendFreq").text(function(d){ return d3.format(",")(d.freq);});
+
+          // update the percentage column.
+          l.select(".legendPerc").text(function(d){ return getLegend(d,nD);});        
+      }
+      
+      function getLegend(d,aD){ // Utility function to compute percentage.
+          return d3.format("%")(d.freq/d3.sum(aD.map(function(v){ return v.freq; })));
+      }
+
+      return leg;
+  }    
 
   function getSubnet(address) {
     let parts = address.split('.');
@@ -249,20 +362,16 @@ function update(data) {
 
   function updateInfo(d){
 
-    // update UI
-    let texturl = 'texts/' + d3.format('06')(d.id) + '.txt'
-    d3.text(texturl, (error,data) => {
+	    // calculate the targetAddress
+	    fData = d.targetAddress.map(function(d){ 
+	        return {type:d[0], freq:d[1]}; 
+	    });  	  
+	  
+	   // update UI
+		var pCData = pieChart(pieChartIDName,fData) // create the pie-chart.
+		var leg    = legend(legendIDName,fData);    // create the legend.
 
-      let maxlength = 700
-      let catalog_text = '<p>' + (data.length > maxlength) ? data.substr(0,maxlength) + '...' : data + '</p>'
-      catalog_text    += '<p><a target="_blank" href="http://archive.aec.at/prix/#' + d.id + '">Ars Electronica link </a></p>'
-      d3.select('#catalog_text').html(catalog_text)}
-    )
-
-    let imgurl = `images/${(''+d.id).padStart(6,'0')}.jpg`;
-    d3.select('#artworkimage').attr('src',imgurl)
-
-    d3.select('#title').text(d.sourceAddress)
+		d3.select('#title').text("Source Address: " + d.sourceAddress)
   }
 
   if( location.hash.length != '' ){
